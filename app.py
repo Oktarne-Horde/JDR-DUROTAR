f1990d24b45b3fe6ee33ac192ea222f969659916
Oktarne-Horde <oktarnehorde@gmail.com>
2026-04-01 16:53:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from orc_generator import generer_orc
 
-st.title("Générateur d'Orc")
+st.title("🧌 Générateur d'Orc")
 
-if st.button("Générer un Orc"):
-    resultat = generer_orc()
-    st.text(resultat)
+# Stockage du résultat
+if "orc" not in st.session_state:
+    st.session_state.orc = ""
+
+# Bouton
+if st.button("🎲 Générer un Orc"):
+    st.session_state.orc = generer_orc()
+
+# Affichage
+if st.session_state.orc:
+    st.code(st.session_state.orc)
